61fd38f770fb1b635fe486a6670381061424ff80
Ryo Aoyama <r.fe51028.r@gmail.com>
2019-10-04 16:37:47 +0900

    Add Package.swift (#53)

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "MagazineLayout",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(name: "MagazineLayout", targets: ["MagazineLayout"])
+    ],
+    targets: [
+        .target(
+            name: "MagazineLayout",
+            path: "MagazineLayout"
+        ),
+        .testTarget(
+            name: "MagazineLayoutTests",
+            dependencies: ["MagazineLayout"],
+            path: "Tests"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
