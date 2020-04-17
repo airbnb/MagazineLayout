bef40bf3fdeb77213a32d20a92ede2b886035bdd
Bryan Keller <bryan.keller@airbnb.com>
2020-04-17 15:02:11 -0700

    Add customizable insert / delete animations

--- MagazineLayout/Public/UICollectionViewDelegateMagazineLayout.swift
+++ MagazineLayout/Public/UICollectionViewDelegateMagazineLayout.swift
@@ -15,6 +15,8 @@
 
 import UIKit
 
+// MARK: - UICollectionViewDelegateMagazineLayout
+
 public protocol UICollectionViewDelegateMagazineLayout: UICollectionViewDelegate {
 
   ///   Asks the delegate for the size mode of the specified item.
@@ -134,5 +136,248 @@ public protocol UICollectionViewDelegateMagazineLayout: UICollectionViewDelegate
     layout collectionViewLayout: UICollectionViewLayout,
     insetsForItemsInSectionAtIndex index: Int)
     -> UIEdgeInsets
+
+  ///   Asks the delegate to modify a layout attributes instance so that it represents the initial visual state of an item being inserted via
+  ///   `UICollectionView.insertItems(at:)`.
+  ///
+  ///   The `initialLayoutAttributes` instance is a reference type, and therefore can be modified directly. If the provided
+  ///   layout attributes instance is not changed in the implementation of this function, then the item will be inserted with no animation.
+  ///
+  ///   - Parameters:
+  ///      - collectionView: The collection view using the layout.
+  ///      - collectionViewLayout: The layout requesting the information.
+  ///      - indexPath: The index path of the item.
+  ///      - initialLayoutAttributes: The unmodified layout attributes representing the final visual state of the inserted
+  ///      item. Modify properties on this instance to create an item insert animation.
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    initialLayoutAttributesForInsertedItemAt indexPath: IndexPath,
+    byModifying initialLayoutAttributes: UICollectionViewLayoutAttributes)
+
+  ///   Asks the delegate to modify a layout attributes instance so that it represents the initial visual state of a header being inserted
+  ///   via `UICollectionView.insertSections(_:)`.
+  ///
+  ///   The `initialLayoutAttributes` instance is a reference type, and therefore can be modified directly. If the provided
+  ///   layout attributes instance is not changed in the implementation of this function, then the header will be inserted with no
+  ///   animation.
+  ///
+  ///   - Parameters:
+  ///      - collectionView: The collection view using the layout.
+  ///      - collectionViewLayout: The layout requesting the information.
+  ///      - index: The index of the section containing the header.
+  ///      - initialLayoutAttributes: The unmodified layout attributes representing the final visual state of the inserted
+  ///      header. Modify properties on this instance to create a header insert animation.
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    initialLayoutAttributesForInsertedHeaderInSectionAtIndex index: Int,
+    byModifying initialLayoutAttributes: UICollectionViewLayoutAttributes)
+
+  ///   Asks the delegate to modify a layout attributes instance so that it represents the initial visual state of a footer being inserted
+  ///   via `UICollectionView.insertSections(_:)`.
+  ///
+  ///   The `initialLayoutAttributes` instance is a reference type, and therefore can be modified directly. If the provided
+  ///   layout attributes instance is not changed in the implementation of this function, then the footer will be inserted with no
+  ///   animation.
+  ///
+  ///   - Parameters:
+  ///      - collectionView: The collection view using the layout.
+  ///      - collectionViewLayout: The layout requesting the information.
+  ///      - index: The index of the section containing the footer.
+  ///      - initialLayoutAttributes: The unmodified layout attributes representing the final visual state of the inserted
+  ///      footer. Modify properties on this instance to create a footer insert animation.
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    initialLayoutAttributesForInsertedFooterInSectionAtIndex index: Int,
+    byModifying initialLayoutAttributes: UICollectionViewLayoutAttributes)
+
+  ///   Asks the delegate to modify a layout attributes instance so that it represents the initial visual state of a background being
+  ///   inserted via `UICollectionView.insertSections(_:)`.
+  ///
+  ///   The `initialLayoutAttributes` instance is a reference type, and therefore can be modified directly. If the provided
+  ///   layout attributes instance is not changed in the implementation of this function, then the background will be inserted with no
+  ///   animation.
+  ///
+  ///   - Parameters:
+  ///      - collectionView: The collection view using the layout.
+  ///      - collectionViewLayout: The layout requesting the information.
+  ///      - index: The index of the section containing the background.
+  ///      - initialLayoutAttributes: The unmodified layout attributes representing the final visual state of the inserted
+  ///      background. Modify properties on this instance to create a background insert animation.
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    initialLayoutAttributesForInsertedBackgroundInSectionAtIndex index: Int,
+    byModifying initialLayoutAttributes: UICollectionViewLayoutAttributes)
+
+  ///   Asks the delegate to modify a layout attributes instance so that it represents the final visual state of an item being removed via
+  ///   `UICollectionView.deleteItems(at:)`.
+  ///
+  ///   The `finalLayoutAttributes` instance is a reference type, and therefore can be modified directly. If the provided
+  ///   layout attributes instance is not changed in the implementation of this function, then the item will be removed with no
+  ///   animation.
+  ///
+  ///   - Parameters:
+  ///      - collectionView: The collection view using the layout.
+  ///      - collectionViewLayout: The layout requesting the information.
+  ///      - indexPath: The index path of the item.
+  ///      - finalLayoutAttributes: The unmodified layout attributes representing the final visual state of the removed item.
+  ///      Modify properties on this instance to create an item delete animation.
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    finalLayoutAttributesForRemovedItemAt indexPath: IndexPath,
+    byModifying finalLayoutAttributes: UICollectionViewLayoutAttributes)
+
+  ///   Asks the delegate to modify a layout attributes instance so that it represents the final visual state of a header being removed
+  ///   via `UICollectionView.deleteSections(_:)`.
+  ///
+  ///   The `finalLayoutAttributes` instance is a reference type, and therefore can be modified directly. If the provided
+  ///   layout attributes instance is not changed in the implementation of this function, then the header will be removed with no
+  ///   animation.
+  ///
+  ///   - Parameters:
+  ///      - collectionView: The collection view using the layout.
+  ///      - collectionViewLayout: The layout requesting the information.
+  ///      - index: The index of the section containing the header.
+  ///      - finalLayoutAttributes: The unmodified layout attributes representing the final visual state of the removed
+  ///      header. Modify properties on this instance to create a header delete animation.
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    finalLayoutAttributesForRemovedHeaderInSectionAtIndex index: Int,
+    byModifying finalLayoutAttributes: UICollectionViewLayoutAttributes)
+
+  ///   Asks the delegate to modify a layout attributes instance so that it represents the final visual state of a footer being removed
+  ///   via `UICollectionView.deleteSections(_:)`.
+  ///
+  ///   The `finalLayoutAttributes` instance is a reference type, and therefore can be modified directly. If the provided
+  ///   layout attributes instance is not changed in the implementation of this function, then the footer will be removed with no
+  ///   animation.
+  ///
+  ///   - Parameters:
+  ///      - collectionView: The collection view using the layout.
+  ///      - collectionViewLayout: The layout requesting the information.
+  ///      - index: The index of the section containing the footer.
+  ///      - finalLayoutAttributes: The unmodified layout attributes representing the final visual state of the removed
+  ///      footer. Modify properties on this instance to create a footer delete animation.
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    finalLayoutAttributesForRemovedFooterInSectionAtIndex index: Int,
+    byModifying finalLayoutAttributes: UICollectionViewLayoutAttributes)
+
+  ///   Asks the delegate to modify a layout attributes instance so that it represents the final visual state of a background being
+  ///   removed via `UICollectionView.deleteSections(_:)`.
+  ///
+  ///   The `finalLayoutAttributes` instance is a reference type, and therefore can be modified directly. If the provided
+  ///   layout attributes instance is not changed in the implementation of this function, then the background will be removed with no
+  ///   animation.
+  ///
+  ///   - Parameters:
+  ///      - collectionView: The collection view using the layout.
+  ///      - collectionViewLayout: The layout requesting the information.
+  ///      - index: The index of the section containing the background.
+  ///      - finalLayoutAttributes: The unmodified layout attributes representing the final visual state of the removed
+  ///      background. Modify properties on this instance to create a background delete animation.
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    finalLayoutAttributesForRemovedBackgroundInSectionAtIndex index: Int,
+    byModifying finalLayoutAttributes: UICollectionViewLayoutAttributes)
+
+}
+
+// MARK: Default Insert Animations
+
+public extension UICollectionViewDelegateMagazineLayout {
+
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    initialLayoutAttributesForInsertedItemAt indexPath: IndexPath,
+    byModifying initialLayoutAttributes: UICollectionViewLayoutAttributes)
+  {
+    // The default insert animation is a simple fade-in.
+    initialLayoutAttributes.alpha = 0
+  }
+
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    initialLayoutAttributesForInsertedHeaderInSectionAtIndex index: Int,
+    byModifying initialLayoutAttributes: UICollectionViewLayoutAttributes)
+  {
+    // The default insert animation is a simple fade-in.
+    initialLayoutAttributes.alpha = 0
+  }
+
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    initialLayoutAttributesForInsertedFooterInSectionAtIndex index: Int,
+    byModifying initialLayoutAttributes: UICollectionViewLayoutAttributes)
+  {
+    // The default insert animation is a simple fade-in.
+    initialLayoutAttributes.alpha = 0
+  }
+
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    initialLayoutAttributesForInsertedBackgroundInSectionAtIndex index: Int,
+    byModifying initialLayoutAttributes: UICollectionViewLayoutAttributes)
+  {
+    // The default insert animation is a simple fade-in.
+    initialLayoutAttributes.alpha = 0
+  }
+
+}
+
+// MARK: Default Delete Animations
+
+public extension UICollectionViewDelegateMagazineLayout {
+
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    finalLayoutAttributesForRemovedItemAt indexPath: IndexPath,
+    byModifying finalLayoutAttributes: UICollectionViewLayoutAttributes)
+  {
+    // The default insert animation is a simple fade-out.
+    finalLayoutAttributes.alpha = 0
+  }
+
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    finalLayoutAttributesForRemovedHeaderInSectionAtIndex index: Int,
+    byModifying finalLayoutAttributes: UICollectionViewLayoutAttributes)
+  {
+    // The default insert animation is a simple fade-out.
+    finalLayoutAttributes.alpha = 0
+  }
+
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    finalLayoutAttributesForRemovedFooterInSectionAtIndex index: Int,
+    byModifying finalLayoutAttributes: UICollectionViewLayoutAttributes)
+  {
+    // The default insert animation is a simple fade-out.
+    finalLayoutAttributes.alpha = 0
+  }
+
+  func collectionView(
+    _ collectionView: UICollectionView,
+    layout collectionViewLayout: UICollectionViewLayout,
+    finalLayoutAttributesForRemovedBackgroundInSectionAtIndex index: Int,
+    byModifying finalLayoutAttributes: UICollectionViewLayoutAttributes)
+  {
+    // The default insert animation is a simple fade-out.
+    finalLayoutAttributes.alpha = 0
+  }
 
 }
