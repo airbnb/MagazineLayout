352b66fd6d3ff1b39b6fbfb40a8cbd1948ed1077
Bryn Bodayle <bryn.bodayle@airbnb.com>
2025-06-20 13:01:21 -0700

    Remove duplicate header

--- Tests/ScreenPixelAlignmentTests.swift
+++ Tests/ScreenPixelAlignmentTests.swift
@@ -1,9 +1,6 @@
 // Created by Bryan Keller on 5/12/22.
 // Copyright © 2022 Airbnb Inc. All rights reserved.
 
-// Created by Bryan Keller on 3/31/20.
-// Copyright © 2020 Airbnb Inc. All rights reserved.
-
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
